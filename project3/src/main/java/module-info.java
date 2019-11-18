788ef9690a71b19f3265a53e5ee59dbd7f9615ee
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-11-18 13:19:48 +0800

    updated

--- project3/src/main/java/module-info.java
+++ project3/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module projects {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires com.almasb.fxgl.entity;
+    requires com.almasb.fxgl.all;
 //    opens proB;
 
     opens proC.viewController;
